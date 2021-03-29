b09022594287c1346e4cd9933b2f022f4284013e
Munir Mahmud <munirmahmud6@gmail.com>
2021-03-29 19:30:27 +0600

    DOM Manipulation

---
+++ Day-8/app.js
@@ -0,0 +1,18 @@
+// const body = document.body;
+// const cardWrapper = body.querySelector('.card-wrapper');
+// const card = body.querySelector('.card');
+
+// body.style.backgroundColor = "#ddd";
+// body.style.margin = 0;
+// body.style.height = "100vh";
+
+// cardWrapper.style.display = "flex";
+// cardWrapper.style.justifyContent = "center";
+// cardWrapper.style.alignItems = "center";
+// cardWrapper.style.height = "100%";
+
+// card.style.backgroundColor = "pink";
+// card.style.width = "400px";
+// card.style.padding = "20px";
+// card.style.borderRadius = "4px";
+
